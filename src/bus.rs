b77423873ec6e95f8223fc2135f342b9971d10ba
Sam Hood <samjdhood@gmail.com>
2025-10-19 15:01:20 -0500

    implement read and write for bus

--- src/bus.rs
+++ src/bus.rs
@@ -20,5 +20,31 @@ impl MemoryBus {
             interrupt_enable: 0,
         }
     }
+
+    pub fn write(&mut self, addr: usize, data: u8) {
+        match addr {
+            0x8000..0x9FFF => self.vram[addr] = data,
+            0xC000..0xDFFF => {
+                // all writes to working ram must be mirror to echo ram
+                self.wram[addr] = data;
+                self.eram[addr] = data;
+            },
+            0xFE00..0xFE9F => self.oam[addr] = data,
+            0xFF00..0xFF7F => self.io_reg[addr] = data,
+            0xFF80..0xFFFe => self.hram[addr] = data,
+            _ => panic!("Unrecognized write address: {}", addr),
+        }
+    }
+
+    pub fn read(&mut self, addr: usize) -> u8 {
+        match addr {
+            0x8000..0x9FFF => self.vram[addr],
+            0xC000..0xDFFF => self.wram[addr],
+            0xFE00..0xFE9F => self.oam[addr],
+            0xFF00..0xFF7F => self.io_reg[addr],
+            0xFF80..0xFFFe => self.hram[addr],
+            _ => panic!("Unrecognized read address: {}", addr),
+        }
+    }
 }
 
