d7919306c2f845d9ace5511056e8c3790f3a8cd7
Sam Hood <samjdhood@gmail.com>
2025-10-19 16:11:51 -0500

    add inclusive ranges, read in files, and test strip_header

--- src/bus.rs
+++ src/bus.rs
@@ -23,26 +23,26 @@ impl MemoryBus {
 
     pub fn write(&mut self, addr: usize, data: u8) {
         match addr {
-            0x8000..0x9FFF => self.vram[addr] = data,
-            0xC000..0xDFFF => {
+            0x8000..=0x9FFF => self.vram[addr] = data,
+            0xC000..=0xDFFF => {
                 // all writes to working ram must be mirror to echo ram
                 self.wram[addr] = data;
                 self.eram[addr] = data;
             },
-            0xFE00..0xFE9F => self.oam[addr] = data,
-            0xFF00..0xFF7F => self.io_reg[addr] = data,
-            0xFF80..0xFFFe => self.hram[addr] = data,
+            0xFE00..=0xFE9F => self.oam[addr] = data,
+            0xFF00..=0xFF7F => self.io_reg[addr] = data,
+            0xFF80..=0xFFFe => self.hram[addr] = data,
             _ => panic!("Unrecognized write address: {}", addr),
         }
     }
 
     pub fn read(&mut self, addr: usize) -> u8 {
         match addr {
-            0x8000..0x9FFF => self.vram[addr],
-            0xC000..0xDFFF => self.wram[addr],
-            0xFE00..0xFE9F => self.oam[addr],
-            0xFF00..0xFF7F => self.io_reg[addr],
-            0xFF80..0xFFFe => self.hram[addr],
+            0x8000..=0x9FFF => self.vram[addr],
+            0xC000..=0xDFFF => self.wram[addr],
+            0xFE00..=0xFE9F => self.oam[addr],
+            0xFF00..=0xFF7F => self.io_reg[addr],
+            0xFF80..=0xFFFe => self.hram[addr],
             _ => panic!("Unrecognized read address: {}", addr),
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,22 @@ mod cartridge;
 mod cpu;
 mod bus;
 
-// TODO: the gb cpu is little endian, so when we are reading any 16bit value, we must remember that the least significant bit is stored first.
-// for the most part, this won't change a lot of how the code works, but for any instructions that use any 16bit values, we will need to account for
-// the endianess.
+
+use std::env;
+use std::fs::File;
+use std::io::Read;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let res: cartridge::Header;
+    let mut file = File::open(&args[1]).expect("File not found");
+    let mut data = Vec::new();
+    if args.len() < 2 {
+        println!("Please supply a file name");
+        return;
+    }
+
+    file.read_to_end(&mut data).expect("Failed to read file");
+    res = cartridge::strip_header(&data);
+    println!("{}", res);
 }
